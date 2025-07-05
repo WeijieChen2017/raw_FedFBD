229aa1a2f9dc97abf2483b874a5136c0f784afdb
WeijieChen2017 <weijiechen1994@gmail.com>
2025-07-05 17:26:46 -0500

    tau training

--- batch_train_tau.py
+++ batch_train_tau.py
@@ -3,7 +3,7 @@ import os
 alpha = [0.1, 0.25, 0.5, 1.0]
 
 for a in alpha:
-    command = f"python3 generate_hetero_datasets.py --dermamnist --alpha {a}"
+    command = f"python3 generate_hetero_datasets.py --dataset dermamnist --alpha {a}"
     print("running: ", command)
     os.system(command)
     command = f"python fbd_main_tau.py --experiment dermamnist --model_flag resnet18 --reg none --FedAvg --save_affix _alpha_{a}"
