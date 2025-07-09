56ef5d96b7f326a8ba01122e78c6654d13349c8c
WeijieChen2017 <weijiechen1994@gmail.com>
2025-07-09 02:03:44 -0500

    aggregate tau

--- aggregate_tau.py
+++ aggregate_tau.py
@@ -47,26 +47,30 @@ for i_row, folder in enumerate(folder_list):
         sheet.cell(row=i_row+2, column=3).value = str(FA)
 
         train_results = os.path.join(folder, "eval_results", "server_evaluation_history.json")
-        with open(train_results, "r") as f:
-            train_results = json.load(f)
-        
-        len_rounds = len(train_results)
-        sheet.cell(row=i_row+2, column=4).value = len_rounds
-        for i_round, round_results in enumerate(train_results):
-            start_idx = 5 + i_round * 7
-            sheet.cell(row=i_row+2, column=start_idx+i_round).value = round_results["round"]
-            M_auc = []
-            M_acc = []
-            for i_M in range(6):
-                M_auc.append(round_results[f"M{i_M}"]["test_auc"])
-                M_acc.append(round_results[f"M{i_M}"]["test_acc"])
+        try:
+            with open(train_results, "r") as f:
+                train_results = json.load(f)
+            
+            len_rounds = len(train_results)
+            sheet.cell(row=i_row+2, column=4).value = len_rounds
+            for i_round, round_results in enumerate(train_results):
+                start_idx = 5 + i_round * 7
+                sheet.cell(row=i_row+2, column=start_idx+i_round).value = round_results["round"]
+                M_auc = []
+                M_acc = []
+                for i_M in range(6):
+                    M_auc.append(round_results[f"M{i_M}"]["test_auc"])
+                    M_acc.append(round_results[f"M{i_M}"]["test_acc"])
 
-            sheet.cell(row=i_row+2, column=start_idx+i_round+1).value = np.mean(M_auc)
-            sheet.cell(row=i_row+2, column=start_idx+i_round+2).value = np.mean(M_acc)
-            sheet.cell(row=i_row+2, column=start_idx+i_round+3).value = round_results[f"averaging"]["test_auc"]
-            sheet.cell(row=i_row+2, column=start_idx+i_round+4).value = round_results[f"averaging"]["test_acc"]
-            sheet.cell(row=i_row+2, column=start_idx+i_round+5).value = round_results[f"ensemble"]["test_auc"]
-            sheet.cell(row=i_row+2, column=start_idx+i_round+6).value = round_results[f"ensemble"]["test_acc"]
+                sheet.cell(row=i_row+2, column=start_idx+i_round+1).value = np.mean(M_auc)
+                sheet.cell(row=i_row+2, column=start_idx+i_round+2).value = np.mean(M_acc)
+                sheet.cell(row=i_row+2, column=start_idx+i_round+3).value = round_results[f"averaging"]["test_auc"]
+                sheet.cell(row=i_row+2, column=start_idx+i_round+4).value = round_results[f"averaging"]["test_acc"]
+                sheet.cell(row=i_row+2, column=start_idx+i_round+5).value = round_results[f"ensemble"]["test_auc"]
+                sheet.cell(row=i_row+2, column=start_idx+i_round+6).value = round_results[f"ensemble"]["test_acc"]
+        except:
+            print(f"No train results found for {folder}")
+            continue
 
 
 workbook.save('tau.xlsx')
